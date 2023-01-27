aa7d0332e23a34656732b498e303a3453ecaaaf0
TochTV <ne01kit@gmail.com>
2023-01-27 08:27:41 +0300

    created

---
+++ src/components/SectionApp.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <section>
+        <plus_app/>
+    </section>
+</template>
+<script>
+import PlusApp from './PlusApp.vue';
+console.log(PlusApp);
+export default {
+    name:'SectionApp',
+    component:{
+        PlusApp
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PlusApp from '@/components/PlusApp.vue'
+import '@/assets/css/style.scss'
 
+
+createApp(App).component('plus-app', PlusApp)
 createApp(App).mount('#app')
